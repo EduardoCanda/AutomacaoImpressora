3a6f54425e45433e39c759c4484008d336511b76
Eduardo Canda <eduardocandac@gmail.com>
2022-05-25 23:27:26 -0300

    Add logger no serviço de arquivos

--- app/src/services/arquivo_service.py
+++ app/src/services/arquivo_service.py
@@ -4,17 +4,21 @@ from os import listdir
 from os.path import isfile, join
 from typing import List
 
+from src.services.logger_service import Logger
+
 
 class ArquivoService:
     def __init__(
             self,
-            pasta_imprimir,
-            pasta_impressos
+            pasta_imprimir: str,
+            pasta_impressos: str,
+            logger: Logger
     ):
         self.arquivos = []
         self.arquivos_path = []
         self.pasta_imprimir = pasta_imprimir
         self.pasta_impressos = pasta_impressos
+        self.logger = logger
 
     def carregar_caminho_arquivos(self) -> List[str]:
         self.arquivos = [
@@ -27,7 +31,13 @@ class ArquivoService:
             f'{self.pasta_imprimir}/{arquivo}' for arquivo in self.arquivos
         ]
 
-        print(f'Caminho dos arquivos carregados: {self.arquivos_path}')
+        self.logger.gravar_log(
+            f'Caminho dos arquivos carregados: {self.arquivos_path}'
+        )
+
+        if not self.arquivos_path:
+            self.logger.gravar_log('Não há arquivos para imprimir.')
+            self.logger.gravar_log('Fim do programa.')
 
         return self.arquivos_path
 
@@ -37,4 +47,6 @@ class ArquivoService:
             destino = f'{self.pasta_impressos}/{arquivo}'
             shutil.move(origem, destino)
 
-        print(f'Arquivos {self.arquivos} movidos para a pasta {self.pasta_impressos}')
+        self.logger.gravar_log(
+            f'Arquivos {self.arquivos} movidos para a pasta {self.pasta_impressos}'
+        )
