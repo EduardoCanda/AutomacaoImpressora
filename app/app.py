c2769e0ff6b51718357632b1198402c6e24d8514
Eduardo Canda <eduardocandac@gmail.com>
2022-05-26 01:50:54 -0300

    Adaptando o fluxo para a criação das pastas, caso necessário

--- app/app.py
+++ app/app.py
@@ -2,28 +2,44 @@ from src.services.logger_service import Logger
 from src.services.arquivo_service import ArquivoService
 from src.services import impressora_service
 
-from webdriver_manager.chrome import ChromeDriverManager
+from src.utils.utils import criar_pastas_caso_nao_exista
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-PASTA_IMPRIMIR = r'C:\Imprimir'
-PASTA_IMPRESSOS = r'C:\Impressos'
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def executar():
+    criou = criar_pastas_caso_nao_exista()
+
     logger = Logger()
+    logger.gravar_log('Iniciando execução...')
+
+    logger.gravar_log('Verificando necessidade de criar as pastas...')
+    if criou:
+        logger.gravar_log('Este computador não tinha uma ou mais pastas criadas para utilizar este programa.')
+        logger.gravar_log('O programa se encarregou de criar as pastas.')
+        logger.gravar_log('De agora em diante, é só utilizar o programa normalmente, pois as pastas foram criadas.')
+        logger.finalizar_log()
+        return
+
+    pasta_imprimir = r'C:\Imprimir'
+    pasta_impressos = r'C:\Impressos'
 
     arquivo_service = ArquivoService(
-        PASTA_IMPRIMIR,
-        PASTA_IMPRESSOS,
+        pasta_imprimir,
+        pasta_impressos,
         logger
     )
 
     caminho_arquivos = arquivo_service.carregar_caminho_arquivos()
 
     if not caminho_arquivos:
+        logger.finalizar_log()
         return
 
+    logger.gravar_log('Conferindo webdriver...')
     service = Service(ChromeDriverManager().install())
 
     impressora_service.imprimir_arquivos(
