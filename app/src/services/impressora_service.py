631af0c7319725295f4d4a1ec3c21c32244ab4aa
Eduardo Canda <eduardocandac@gmail.com>
2022-05-26 22:39:15 -0300

    Ajustando o tempo de espera para impressão dos arquivos

--- app/src/services/impressora_service.py
+++ app/src/services/impressora_service.py
@@ -14,12 +14,21 @@ def imprimir_arquivos(
 ):
     logger.gravar_log('Iniciando serviço de impressão...')
 
+    contador = 0
+
     for caminho in caminho_arquivos:
         # Abrir arquivo no navegador
         webdriver.get(caminho)
 
+        contador += 1
+
         # Aguardar arquivo carregar no navegador
-        time.sleep(3.5)
+        if contador == 1:
+            logger.gravar_log('Imprimindo primeiro arquivo da fila...')
+            logger.gravar_log('Aguardando 30 segundos para o próximo arquivo ser impresso...')
+            time.sleep(30)
+        else:
+            time.sleep(3.5)
 
         # Selecionar ícone da impressora
         pg.hotkey('ctrl', 'p')
